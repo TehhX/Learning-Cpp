4f54b0ff63e8d6ff6b412ee03fa2fda2e86a8c75
Sam <156055395+TehhX@users.noreply.github.com>
2025-02-13 21:49:23 -0500

    Same

--- Blackjack/blackjack_main.cpp
+++ Blackjack/blackjack_main.cpp
@@ -5,65 +5,83 @@ namespace Settings {
     constexpr int stopDrawing { 17 };
 }
 
+bool playerTurn(Deck& deck, int& player) {
+    while (true) {
+        std::cout << "(h) to hit, or (s) to stand: ";
+
+        char in {};
+        std::cin >> in;
+
+        std::cout << '\n';
+
+        if (in == 's')
+            return false;
+        
+        if (in == 'h') {
+            Card playerCard { deck.dealCard() };
+            player += playerCard.value();
+
+            std::cout << "You were dealt " << playerCard << ". You now have " << player << ".\n";
+
+            return true;
+        }
+    }
+}
+
+bool dealerTurn(Deck& deck, int& dealer) {
+    Card dealerCard { deck.dealCard() };
+    dealer += dealerCard.value();
+
+    std::cout << "The dealer flips a " << dealerCard << ". They now have " << dealer << ".\n";
+
+    return true;
+}
+
 int main() {
     Deck deck {};
 
     int dealer { deck.dealCard().value() };
     int player { deck.dealCard().value() };
 
-    std::cout << "The dealer is showing a score of " << dealer <<
-               "\nYou have a score of " << player << '\n';
+    std::cout << "You have a score of " << player <<
+               "\nThe dealer is showing a score of " << dealer << '\n';
+               
+    bool dealerActive { true };
+    bool playerActive { true };
 
-    bool playing { true };
+    while (dealerActive || playerActive) {
+        if (playerActive)
+            playerActive = playerTurn(deck, player);
 
-    while (true) {
-        Card dealerCard { deck.dealCard() };
-        dealer += dealerCard.value();
-
-        while (playing) {
-            std::cout << "(h) to hit, or (s) to stand: ";
-
-            char in {};
-            std::cin >> in;
-
-            std::cout << '\n';
-
-            if (in == 's')
-                playing = false;
-            
-            else if (in == 'h') {
-                Card playerCard { deck.dealCard() };
-                player += playerCard.value();
-
-                std::cout << "You were dealt " << playerCard << ". You now have " << player << ".\n";
-
-                break;
-            }
+        if (player == Settings::blackjack) {
+            std::cout << "You got blackjack!\n";
+            return 0;
         }
 
-        std::cout << "The dealer flips a " << dealerCard << ". They now have " << dealer << ".\n";
+        if (player > Settings::blackjack) {
+            std::cout << "You went bust!\n";
+            return 0;
+        }
 
+        if (dealerActive)
+            dealerActive = dealerTurn(deck, dealer);
+
+        if (dealer == Settings::blackjack) {
+            std::cout << "The dealer got blackjack!\n";
+            return 0;
+        }
+    
+        if (dealer > Settings::blackjack) {
+            std::cout << "The dealer went bust!\n";
+            return 0;
+        }
+            
         if (dealer > Settings::stopDrawing)
-            break;
-
-        if (player > Settings::blackjack)
-            break;
+            dealerActive = false;
     }
 
     if (dealer == player)
         std::cout << "You tied!\n";
-
-    else if (dealer == Settings::blackjack)
-        std::cout << "The dealer got blackjack!\n";
-
-    else if (player == Settings::blackjack)
-        std::cout << "You got blackjack!\n";
-
-    else if (dealer > Settings::blackjack)
-        std::cout << "The dealer went bust!\n";
-
-    else if (player > Settings::blackjack)
-        std::cout << "You went bust!\n";
 
     else if (player > dealer)
         std::cout << "You win!\n";
